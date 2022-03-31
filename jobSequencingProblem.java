6ada512f4534912319d64e147ed7a49f83db16cc
V15hnu24 <799vishnu530@gmail.com>
2022-04-01 04:06:52 +0530

    Job sequencing gfg

--- jobSequencingProblem.java
+++ jobSequencingProblem.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.TreeMap;
+import java.util.*;
 
 class Solution
 {
@@ -31,39 +30,72 @@ class Solution
         return dp.lastEntry().getValue();
     }
 
-    //Function to find the maximum profit and the number of jobs done.
+    //Function to find the maximum profit and the number of jobs done. NlonN
 
     int[] JobScheduling(Job arr[], int n)
     {
-        Arrays.sort(arr,(a,b) -> a.profit - b.profit);
-        int[] ary = new int[n+1];
-        Arrays.fill(ary,-1);
-
-        for(int i = n - 1;i>=0;i--){
-            int k = arr[i].deadline;
-            if(ary[k] == -1 ){
-                ary[k] = arr[i].profit;
+        Arrays.sort(arr, new Comparator<Job>(){
+            public int compare(Job j1,Job j2){
+                return j1.deadline - j2.deadline;
             }
-            else{
-                while(ary[k]!=-1){
-                    k--;
-                }
-                ary[k] = arr[i].profit;
+        });
+
+        ArrayList<Job> ans_aryList = new ArrayList<>();
+        int[] ans = new int[]{0,0};
+        PriorityQueue<Job> heap = new PriorityQueue<>(
+                (a,b) -> {return b.profit - a.profit;});
+
+        int slots = arr[arr.length-1].deadline;
+
+        for (int i = n-1; i > -1 ; i--) {
+            int slots;
+
+            if (i==0){
+                slots = arr[i].deadline;
+            }else{
+                slots = arr[i].deadline - arr[i-1].deadline;
+            }
+
+            heap.add(arr[i]);
+
+            while (slots>0 && heap.size() > 0){
+
+                Job job = heap.remove();
+                slots--;
+
+                ans_aryList.add(job);
             }
         }
-
-        int num = 0, max_profit = 0;
-
-        for(int i  = 1; i<n+1;i++){
-            if(ary[i]!=-1){
-                num++;
-                max_profit += ary[i];
-            }
+        ans[0] = ans_aryList.size();
+        for (Job j: ans_aryList) {
+            ans[1] = ans[1] + j.profit;
         }
 
-        int temp[] = new int[]{num,max_profit};
-        return temp;
+        return ans;
     }
+
+
+    //N2 solution
+    int[] job_sequencing(Job arr[], int n){
+        Arrays.sort(arr, (a,b) -> b.profit - a.profit);
+        boolean[] res = new boolean[n];
+        int[] job = new int[n];
+
+        for (int i = 0; i<arr.length; i++) {
+
+            for (int j = Math.min(n-1,arr[i].deadline-1); j >=0 ; j--) {
+                if(res[j] == false){
+                    res[j] = true;
+
+                    job[i] = arr[i].id;
+                    break;
+                }
+            }
+        }
+
+        return job;
+    }
+
     class Job{
         int id,deadline,profit;
         public Job(int a,int b,int c){
