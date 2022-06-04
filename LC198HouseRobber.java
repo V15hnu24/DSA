a04baa2d2d07cd0c9bbc7437dcb6a5fb15830cc8
V15hnu24 <799vishnu530@gmail.com>
2022-06-04 23:03:43 +0530

    Lc 198 House robber

--- LC198HouseRobber.java
+++ LC198HouseRobber.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class LC198HouseRobber {
+public class LC198HouseRobber {
+    public static void main(String[] args) {
+
+    }
+
+    public int rob(int[] nums) {
+        int dp[] = new int[nums.length + 2];
+
+        dp[nums.length] = 0;
+        dp[nums.length + 1] = 0;
+
+        for(int i = nums.length-1; i>=0; i--){
+            dp[i] = Math.max(nums[i] + dp[i+2], dp[i+1]);
+        }
+
+        return dp[0];
+    }
+    // Optimised SC to O(1)
+    public int rob_updated(int[] nums) {
+
+        int a = 0;
+        int b = 0;
+
+        for(int i = nums.length-1; i>=0; i--){
+            int temp1 = nums[i] + b;
+            int temp2 = a;
+
+            int c = Math.max(temp1, temp2);
+            b = a;
+            a = c;
+        }
+
+        return a;
+    }
 }
